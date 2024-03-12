6b9a22243ee0b9cb0f6883047011d96c87915978
unknown <subekshad2019@gmail.comgit config --global user.name subekshya0-github>
2024-03-12 22:26:38 +0545

    2358900_SubekshyaDhamala_NMC

---
+++ Task1/Task1.c
@@ -0,0 +1,74 @@
+// Task 1 C-Programming Assignment
+// Student Id : 2358900
+// Name : Subekshya Dhamala
+
+
+
+#include <stdio.h>
+#include <math.h> 
+#include <stdlib.h>
+
+// Function to read data from file and accumulate sums
+void read_data(FILE *fptr, float *sum_X, float *sum_Y, float *sum_XY, float *sum_Xsq, int *n);
+
+int main() {
+    // File pointers for four datasets
+    FILE *fptr[4];
+    fptr[0] = fopen("datasetLR1.txt", "r");
+    fptr[1] = fopen("datasetLR2.txt", "r");
+    fptr[2] = fopen("datasetLR3.txt", "r");
+    fptr[3] = fopen("datasetLR4.txt", "r");
+
+    // Initialize variables for sums and calculations
+    int n = 0;
+    float sum_X = 0;
+    float sum_Y = 0;
+    float sum_XY = 0;
+    float sum_Xsq = 0;
+    float calc_A, calc_B, y_Pred, user_x;
+    int i;
+
+    // Loop through files, read data, and accumulate sums
+    for (i = 0; i < 4; i++)
+        read_data(fptr[i], &sum_X, &sum_Y, &sum_XY, &sum_Xsq, &n);
+
+    // Calculate linear regression coefficients using math functions
+    calc_A = ((sum_Y * sum_Xsq) - (sum_X * sum_XY)) / ((n * sum_Xsq) - pow(sum_X, 2));
+    calc_B = ((n * sum_XY) - (sum_X * sum_Y)) / ((n * sum_Xsq) - pow(sum_X, 2));
+
+    // Print results
+    printf("A = %0.2f\n", calc_A);
+   
+    printf("B = %0.2f\n", calc_B);
+
+    printf("The linear equation obtained from the given calculation is : %0.2fx + %0.2f\n", calc_B, calc_A);
+
+    // Prompt user for input and predict y
+    printf("\nEnter x coordinate to predict y: ");
+    scanf("%f", &user_x);
+    y_Pred = calc_B * user_x + calc_A;
+
+    // Display prediction results
+    printf("The value of x = %0.2f", user_x);
+    printf("\nThe predicted result is y = %0.2f\n", y_Pred);
+
+    // Close all file pointers
+    for (i = 0; i < 4; i++)
+        fclose(fptr[i]);
+
+    return 0;
+}
+
+// Function to read data from file and accumulate sums
+void read_data(FILE *fptr, float *sum_X, float *sum_Y, float *sum_XY, float *sum_Xsq, int *n) {
+    int x, y;
+    // Read data until end of file is reached
+    while (fscanf(fptr, "%d, %d", &x, &y) != EOF) {
+        (*sum_X) += x;
+        (*sum_Y) += y;
+        (*sum_Xsq) += pow(x, 2);
+        (*sum_XY) += (x * y);
+        (*n)++;
+    }
+}
+
